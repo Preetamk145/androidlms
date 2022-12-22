55bea9ba90e0d3de9d759528becb63bb0708cb1b
ashwiz-kotian <ashwizkotian@gmail.com>
2022-12-22 08:56:55 +0530

    commited

--- app/src/main/java/com/example/lmsnew/Takebook.java
+++ app/src/main/java/com/example/lmsnew/Takebook.java
@@ -1,14 +1,59 @@
 package com.example.lmsnew;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Takebook extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class Takebook extends AppCompatActivity {
+    EditText removebookusn,removebookid;
+    Button takebook;
+    BottomNavigationView bootomnavbar;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_takebook);
+        removebookusn=findViewById(R.id.removebookusn);
+        removebookid=findViewById(R.id.removebookid);
+        takebook=findViewById(R.id.removebook);
+        bootomnavbar=findViewById(R.id.bottomNavigationView);
+        bootomnavbar.setSelectedItemId(R.id.givebook);
+        takebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        bootomnavbar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId())
+                {
+                    case R.id.givebook:
+                        return  true;
+                    case R.id.user:
+                        startActivity(new Intent(getApplicationContext(),add_user.class));
+                        overridePendingTransition(0,0);
+                        return  true;
+                    case R.id.booklist:
+                        startActivity(new Intent(getApplicationContext(),booklist.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+
+                }
+                return false;
+            }
+        });
+
     }
 }
